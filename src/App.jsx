c3b6e8e983369db2815b7419d8215d0cd6bee068
Benjamin <benjamin.cerninak@gmail.com>
2024-08-04 07:44:49 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p>Click on the Vite and React logos to learn more</p>
+      <Button variant="success">Ahoj</Button>
+    </>
+  );
+}
+
+export default App;
