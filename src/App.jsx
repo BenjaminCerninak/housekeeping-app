2d8aee78d3bcb9485bfeb242200a30961c49434c
Michaela Cerninakova <michaela.cerninakova@gmail.com>
2024-08-25 17:24:29 +0200

    delete unused components

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,14 @@
-import { useState } from "react";
+
 
 import Button from "react-bootstrap/Button";
 import "bootstrap/dist/css/bootstrap.min.css";
-import ListGroup from "react-bootstrap/ListGroup";
 
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 
-function BasicExample() {
-  return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
-}
 
 function App() {
   return (
     <>
-      <BasicExample />
       <p>Click on the Vite and React logos to learn more</p>
-      <DefaultExample />
       <div class="text-center">
         <Button variant="success">Ahojky</Button>
       </div>
@@ -51,16 +17,6 @@ function App() {
   );
 }
 
-function DefaultExample() {
-  return (
-    <ListGroup>
-      <ListGroup.Item>Cras justo odio</ListGroup.Item>
-      <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-      <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-      <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-      <ListGroup.Item action>Vestibulum at eros</ListGroup.Item>
-    </ListGroup>
-  );
-}
+
 
 export default App;
