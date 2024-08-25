a2461fc2c073033357982295719620738dfc015d
Michaela Cerninakova <michaela.cerninakova@gmail.com>
2024-08-25 10:51:15 +0200

    Add data to housekeepingData

--- src/houseKeepingData.jsx
+++ src/houseKeepingData.jsx
@@ -1,3 +1,3 @@
-export const houseKeepingData = [
-  { title: "Vysávání", interval: 7, startDate: "iso.....", id: 101 },
+export const housekeepingData = [
+  { title: "Vysávání", interval: 7, startDate: "iso.....", id: 101 },{ title: "Dezinfekce košů", interval: 7, startDate: "iso.....", id: 102 },{ title: "Pročistit odpady", interval: 7, startDate: "iso.....", id: 103 },{ title: "Umytí koupelny", interval: 7, startDate: "iso.....", id: 104 },{ title: "Umytí WC", interval: 7, startDate: "iso.....", id: 105 },{ title: "Utření Prachu", interval: 7, startDate: "iso.....", id: 105 }
 ];
